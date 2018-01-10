4c44a1d782c90af1c4c4221695180e1bb1ac6c6e
Emil <emilioag@users.noreply.github.com>
2018-01-10 09:24:58 +0100

    Update setup.py

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name=script,
+    name={{cookiecutter.script_name}},
     version='0.1',
     packages=find_packages(),
     install_requires=[
