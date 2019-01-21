a1fc7fe56b8f36395f7dcaf301dac3d215185f1f
emilioag <agonzalez.emilio@gmail.com>
2019-01-21 09:17:13 +0100

    bufix: add quote

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name={{cookiecutter.script_name}},
+    name='{{cookiecutter.script_name}}',
     version='0.1',
     packages=find_packages(),
     install_requires=[
